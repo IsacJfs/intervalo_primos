3b0d8b7cc14e90b427d213ec6c045cb97b0c9a63
Isac Desktop <isac.jfs@gmail.com>
2023-06-23 18:29:39 -0300

    Implementando a biblioteca Numpy

--- intervalo_primos.py
+++ intervalo_primos.py
@@ -1,21 +1,22 @@
+import numpy as np
 print("Calculador de primos")
 print("Valores muito grandes podem demorar para serem processados")
 
 numero_coletado = int(input("Digite um número inteiro: \n"))    # Coleta o número que vai ser utlizado
-lista_primos = []   # armazena os números primos calculados
-lista_generica = []  # lista que armazena resultado das divisões antes de determinar se o valor é primo
+lista_primos = np.array([])   # armazena os números primos calculados
+lista_generica = np.array([])
+inteiros = np.array([])
 
-for principal in range(numero_coletado, 1, -1):
-    # Laço criado com range que vai do número digitado até 1 com passo de -1
-    lista_generica.clear()  # limpa a lista genérica a cada ciclo do for
-    for secundario in range(2, principal):
-        # laço onde o resto da divisão de todos os números contidos entre dois e o principal e o número secundário
-        # serão armazenados
-        valor = principal % secundario
-        lista_generica.append(valor)
-    if not 0 in lista_generica:
-        # Confere se existe alguma divisão com resto zero, caso não, o número é primo
-        lista_primos.append(principal)
+array_inicial = np.arange(numero_coletado, 1, -1)
 
+for numero in np.arange(len(array_inicial), 2, -1):
+    lista_generica = numero / array_inicial
+    inteiros = np.count_nonzero(lista_generica.astype(int) == lista_generica)
+    if inteiros == 1:
+        lista_primos = np.append(lista_primos, numero)
+
+lista_primos = np.append(lista_primos, [2])
 print(f"Existem {len(lista_primos)} números primos de 2 a {numero_coletado}")
 print(f"São eles : \n{lista_primos}")
+
+
